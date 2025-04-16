38c6fe2a5ba7270b4d8999124a5aa0e59a978b9d
Vy Cao <yenvy.caohoai@gmail.com>
2025-04-16 15:11:36 +0700

    fix bugs in Footer

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -5,7 +5,9 @@ const Footer = () => {
   return (
     <footer className="mt-24 border-t pt-6 px-4 text-sm text-gray-500 dark:text-gray-400">
       <div className="flex flex-col items-center gap-4 text-center">
-        <p>Thank you for visiting. Let's connect and build something impactful.</p>
+        <p>
+          Thank you for visiting. Connect with me and build something impactful.
+        </p>
         <Socials />
         <p className="text-xs">&copy; {new Date().getFullYear()} Vy Cao</p>
       </div>
